259003279eef8bfe04adb784d7cba7c3e71d3734
Sean Gondowardoyo <seangondo0@gmail.com>
2023-01-21 18:50:37 +0700

    Initial commit

---
+++ app/src/main/java/com/tugasakhir/elderlycare/MainActivity.java
@@ -0,0 +1,68 @@
+package com.tugasakhir.elderlycare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+//import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MainActivity extends AppCompatActivity {
+
+    MqttHelper mqttHelper;
+    TextView connections;
+    TextInputEditText user, passw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        user = (TextInputEditText) findViewById(R.id.user_id);
+        passw = (TextInputEditText) findViewById(R.id.user_passw);
+
+        connections = (TextView) findViewById(R.id.tv3);
+        startMqtt();
+    }
+
+    private void startMqtt() {
+        mqttHelper = new MqttHelper(getApplicationContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+                Log.d("Debug", "MASOKKK!");
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+                Log.d("Debug", "KELUAR!");
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug", mqttMessage.toString());
+                connections.setText(mqttMessage.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+    }
+}
