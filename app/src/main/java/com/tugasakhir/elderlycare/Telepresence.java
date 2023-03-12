d2d39f06dffe79964f860b4d22a35fea7f12507d
Sean Gondowardoyo <seangondo0@gmail.com>
2023-03-12 20:08:41 +0700

    Update TELE Move & Neck 3/12/2023

--- app/src/main/java/com/tugasakhir/elderlycare/Telepresence.java
+++ app/src/main/java/com/tugasakhir/elderlycare/Telepresence.java
@@ -1,19 +1,47 @@
 package com.tugasakhir.elderlycare;
 
+import static com.tugasakhir.elderlycare.MainActivity.client;
+import static com.tugasakhir.elderlycare.mqttServices.kitchen_gas;
+import static com.tugasakhir.elderlycare.mqttServices.kitchen_light;
+import static com.tugasakhir.elderlycare.mqttServices.kitchen_no;
+import static com.tugasakhir.elderlycare.mqttServices.living_light;
+import static com.tugasakhir.elderlycare.mqttServices.living_no;
+import static com.tugasakhir.elderlycare.mqttServices.living_temp;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Switch;
+
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+import io.github.controlwear.virtual.joystick.android.JoystickView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Telepresence#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Telepresence extends Fragment {
+public class Telepresence extends Fragment implements View.OnClickListener {
+
+    ImageButton bUp, bDown;
+
+    //Updater
+    Handler handler = new Handler();
+    Runnable runnable;
+    int delay = 1*100;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,5 +88,84 @@ public class Telepresence extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_telepresence, container, false);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        JoystickView joystick = (JoystickView) view.findViewById(R.id.JoystickControl);
+        joystick.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                // do whatever you want
+                try {
+                    client.publish(MainActivity.myUser+"/apps/robot/controller/move", ("{\"angle\": " + angle + ", \"strength\": " + strength + "}").getBytes(),0, false);
+                    //client.publish(MainActivity.myUser+"/apps/robot/map_coord/data", ("{\"x\": " + angle + ", \"y\": " + strength + "}").getBytes(),0, true);
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                Log.d("Joystick Angle", String.valueOf(angle));
+                Log.d("Joystick Strength", String.valueOf(strength));
+            }
+        });
+
+        bUp = (ImageButton) view.findViewById(R.id.teleNeckUp);
+        bDown = (ImageButton) view.findViewById(R.id.teleNeckDown);
+        bUp.setOnClickListener(this);
+        bDown.setOnClickListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        handler.postDelayed(runnable = new Runnable() {
+            @Override
+            public void run() {
+                if(bUp.isPressed()) {
+                    try {
+                        client.publish(MainActivity.myUser+"/apps/robot/controller/neck", ("{\"angle\": \"up\" }").getBytes(),0, false);
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if(bDown.isPressed()) {
+                    try {
+                        client.publish(MainActivity.myUser+"/apps/robot/controller/neck", ("{\"angle\": \"down\" }").getBytes(),0, false);
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    }
+                }
+                handler.postDelayed(runnable, delay);
+            }
+        }, delay);
+
+        super.onResume();
+
+    }
+
+    @Override
+    public void onPause() {
+        handler.removeCallbacks(runnable);
+        super.onPause();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.teleNeckUp:
+                try {
+                    client.publish(MainActivity.myUser+"/apps/robot/controller/neck", ("{\"angle\": \"up\" }").getBytes(),0, false);
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.teleNeckDown:
+                try {
+                    client.publish(MainActivity.myUser+"/apps/robot/controller/neck", ("{\"angle\": \"down\" }").getBytes(),0, false);
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
     }
 }
